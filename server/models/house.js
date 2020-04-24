a3886280440cfdc520236b39a8ebdc833537003f
rodrigoanti <rodrigo.hernandez@live.cl>
2020-04-23 23:07:16 -0400

    Update house.js

--- server/models/house.js
+++ server/models/house.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const HouseSchema = new Schema ({
-    region : { type:String, required:true },
-    city : { type:String, required:true },
-    commune : { type:String, required:true },
-    price : { type:Number, required:true },
-    room : { type:Number, required:true },
-    bathroom_qtity : { type:Number, required:true },
-    id_user : { type:String, required:true },
+    region: { type:String, required:true },
+    city: { type:String, required:true },
+    commune: { type:String, required:true },
+    price: { type:Number, required:true },
+    room: { type:Number, required:true },
+    bathroom_qtity: { type:Number, required:true },
+    id_user : { type:String, required:true }
 })
 
 module.exports = mongoose.model('House', UserSchema);
